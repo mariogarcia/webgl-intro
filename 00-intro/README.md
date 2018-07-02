44aebae22669172ace1365f80f9c0d9c4eac8de1
Mario Garcia <mario.ggar@gmail.com>
2018-07-02 16:13:24 +0200

    Add structure of intro example js

---
+++ 00-intro/README.md
@@ -0,0 +1,5 @@
+## Raw WebGL example
+
+This example draws a simple triangle and square using plain WebGL.
+
+> This example is taken from http://www.jlabstudio.com/webgl/2011/10/webgl-tutorial-1-un-triangulo-y-un-cuadrado/
--- 00-intro/intro.js
+++ 00-intro/intro.js
@@ -1,11 +1,17 @@
+/**
+ * Creates a new WebGL context if the browser supports the WebGL
+ * specification
+ *
+ * @param canvas the canvas to take the WebGL context from
+ * @return a WebGL context if the browser supported it
+ */
 const initWebGL = (canvas) => {
     gl = null;
 
     try {
         // Extracts the webgl context
         gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-    }
-    catch(e) {}
+    } catch(e) {}
 
     // if there is none then show an alert message
     if (!gl) {
@@ -16,14 +22,42 @@ const initWebGL = (canvas) => {
     return gl;
 };
 
+/**
+ * Function that executes the function passed as argument
+ * if the WebGL context passed as first argument exists
+ *
+ * @param webGL the WebGL context
+ * @param function that requires a WebGL context
+ */
+const withWebGL = (webGL, func) => {
+    if (webGL) {
+        func(webGL);
+    }
+};
+
+const initShaders = (webGL) => {
+
+};
+
+const initBuffers = (webGL) => {
+
+};
+
+const drawScene = (webGL, shaders, buffers) => {
+    webGL.clearColor(0.0, 0.0, 0.0, 1.0);
+    webGL.enable(wgl.DEPTH_TEST);
+
+};
+
 const start = () => {
     const canvas = document.getElementById("glcanvas");
-    const gl = initWebGL(canvas);
+    const wglCtx = initWebGL(canvas);
 
-    if (gl) {
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        gl.enable(gl.DEPTH_TEST);
-        gl.depthFunc(gl.LEQUAL);
-        gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-    }
+    withWebGL(wglCtx, (wgl) => {
+
+        const shaders = initShaders(wgl);
+        const buffers = initBuffers(wgl);
+
+        drawScene(wgl, shaders, buffers);
+    })
 };
