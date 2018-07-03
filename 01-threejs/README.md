30998d88fae11d2640e1f1760a4232511db7bbd5
Mario Garcia <mario.ggar@gmail.com>
2018-07-03 14:01:23 +0200

    Add three.js example

--- 01-threejs/README.md
+++ 01-threejs/README.md
@@ -2,4 +2,6 @@
 
 > https://threejs.org/
 
->
+Example that shows a rotating cube
+
+> This is example is taken from https://threejs.org/docs/index.html#manual/introduction/Creating-a-scene
---
+++ 01-threejs/example.js
@@ -0,0 +1,26 @@
+const start = () => {
+    var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+    var canvas = document.getElementById('glcanvas');
+	var renderer = new THREE.WebGLRenderer({ canvas: canvas});
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var material = new THREE.MeshBasicMaterial( { color: 0xcccccc } );
+	var cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+
+	camera.position.z = 3;
+
+	var animate = function () {
+		requestAnimationFrame( animate );
+
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.01;
+
+		renderer.render( scene, camera );
+	};
+
+	animate();
+};
