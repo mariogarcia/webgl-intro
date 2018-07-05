3cce72839ea05597b6674c303d432114b5abe7da
Mario Garcia <mario.ggar@gmail.com>
2018-07-05 15:36:58 +0200

    Add moment.js to convert dates to integers

--- 02-pixijs/00_simple_line/Utils.js
+++ 02-pixijs/00_simple_line/Utils.js
@@ -1,3 +1,10 @@
+/**
+ * Gets a random number between 0 and the number passed as argument
+ *
+ * @param max maximum possible value of the random number
+ * @return a random number between 0 and the value passed as argument
+ * @since 0.1.0
+*/
 const getRandomInt = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
 }
@@ -13,4 +20,17 @@ const createRandomPoints = (maxX, maxY, howMany) => {
     return Immutable
         .List(unordered)
         .sortBy((point) => point.x);
+};
+
+/**
+ * Converts a list of points with the x having a date instead of an
+ * integer to a list of points with the x converted to an integer.
+ *
+ * @param timeFormat format of the date (x axis)
+ * @param points points to be converted
+ * @return a list with the converted points
+ * @since 0.1.0
+ */
+const convertTimeSeries = (timeFormat, points) => {
+
 };
