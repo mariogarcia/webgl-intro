4c6723b6be0ab2cc7177f016ae5dba8fae659f1b
Mario Garcia <mario.ggar@gmail.com>
2018-07-06 15:23:19 +0200

    Add structure of func needed to convert dates

--- 02-pixijs/01_time_series/kal/utils/points.js
+++ 02-pixijs/01_time_series/kal/utils/points.js
@@ -1,4 +1,24 @@
 /**
+ * This class has the x and the y point to be shown in the graph along
+ * with the original information
+ *
+ * @since 0.1.0
+ */
+class Point {
+    /**
+     * Default constructor
+     *
+     * @param pointMap a map with the point information
+     * @since 0.1.0
+     */
+    constructor(pointMap) {
+        this.x = pointMap.x;
+        this.y = pointMap.y;
+        this.source = pointMap
+    }
+}
+
+/**
  * Gets a random number between 0 and the number passed as argument
  *
  * @param max maximum possible value of the random number
@@ -23,6 +43,53 @@ const createRandomPoints = (maxX, maxY, howMany) => {
 };
 
 /**
+ * Returns the difference between the current date and the minimum
+ * date available at the graph representing the x value.
+ *
+ * @param x string representation of a date
+ * @param minX string representation of the minimum date at the graph
+ * @param format format of the string date
+ * @param interval difference unit (hours, days, minutes...)
+ * @return the difference between the two dates
+ * @since 0.1.0
+ */
+const diffX = (x, minX, format, interval) => {
+    const minDate = moment(minX, format);
+    const curDate = moment(x, format);
+
+    curDate.diff(minDate, interval);
+}
+
+/**
+ * @param y
+ * @param minY
+ * @return
+ * @since 0.1.0
+ */
+const diffY = (y, minY) => {
+    return y - minY
+}
+
+/**
+ * Converts a given point with x as a date to its correspondent point
+ *
+ * @param conf information about the data set maxs and mins
+ * @param point the source point
+ * @return a converted point
+ * @since 0.1.0
+ */
+const convertTo = (conf) => (point) => {
+    const diff_x = diffX(point.x, conf.x.min, conf.format, conf.interval)
+    const diff_y = diffY(point.y, conf.y.min)
+
+    return new Point({
+        x: diff_x,
+        y: diff_y,
+        source: point
+    })
+};
+
+/**
  * Converts a list of points with the x having a date instead of an
  * integer to a list of points with the x converted to an integer.
  *
@@ -32,5 +99,27 @@ const createRandomPoints = (maxX, maxY, howMany) => {
  * @since 0.1.0
  */
 const convertTimeSeries = (timeFormat, points) => {
+    const sourceList = List(points)
 
+    const minX = sourceList.min((p) => p.x)
+    const maxX = sourceList.max((p) => p.x)
+    const minY = sourceList.min((p) => p.y)
+    const maxY = sourceList.max((p) => p.y)
+
+    const conf = {
+        format: timeFormat,
+        interval: "days",
+        x: {
+            min: minX,
+            max: maxX
+        },
+        y: {
+            min: minY,
+            max: maxY
+        }
+    }
+
+    return sourceList
+        .map((point) => new Point(point))
+        .map(convertTo(conf))
 };
